664758a83e85ccf3b5a89c750e4eb7b16d203b44
tanat.lokejaroenlarb@prudential.co.th <tanat.lokejaroenlarb@prudential.co.th>
2021-02-06 01:11:23 +0700

    publish from soil

--- program/main-soil.py
+++ program/main-soil.py
@@ -7,12 +7,14 @@ import time
 # system
 import time
 import sys
+import requests
 
 # parameter
 box_number = sys.argv[1]
 gpio_pin = int(sys.argv[2])
 
 GPIO.setmode(GPIO.BCM)
+PROMETHEUS_GATEWAY_URL = "http://54.179.72.202:9091/metrics/job/push_metrics/"
 
 while True:
     #Discharge capacitor
@@ -23,11 +25,18 @@ while True:
     #Count loops until voltage across capacitor reads high on GPIO
     while (GPIO.input(gpio_pin) == GPIO.HIGH):
         print 'wet'
-        time.sleep(5) #in seconds, suspends execution.
+        time.sleep(0.1) #in seconds, suspends execution.
     print 'dry!'
-    #publish readings
-    box = Box(box_number,[Tree(1,1,1,1)])
-    box.publish_box_data()
+
+    print('publishing central data from adapter...')
+    data = 'soil_moisture ' + str(0) + '\n'
+
+    print(data)
+
+    response = requests.post(self.PROMETHEUS_GATEWAY_URL, data=data, headers={'Content-Type': 'application/octet-stream'})
+
+    print('response : ', response)
+    print('content published')
 
 GPIO.cleanup()
 #  soil code ref: https://github.com/jenfoxbot/SoilSensorAPI/blob/master/JenFoxBotSMSV1c.py
