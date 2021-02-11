6665f186ec75e4c5dbeaaf24dc077c38ea06d4b9
tanat.lokejaroenlarb@prudential.co.th <tanat.lokejaroenlarb@prudential.co.th>
2021-02-11 12:41:03 +0700

    add s3 upload program

---
+++ images/s3upload.py
@@ -0,0 +1,23 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+ACCESS_KEY = ''
+SECRET_KEY = ''
+
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+def upload_to_aws(local_file, bucket, s3_file):
+    
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+uploaded = upload_to_aws('./test.txt', 'argitect-iot-demo-1', 'test.txt')
